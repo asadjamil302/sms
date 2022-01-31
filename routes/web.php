a9b4d95a18c954a4885f354575876331a5650e88
fahim8921 <fahim.bilal.ch@gmail.com>
2022-01-31 17:59:58 +0500

    changes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\StudentController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::resource('student', [StudentController::class, 'create']);
+Route::resource('student', StudentController::class);
+
