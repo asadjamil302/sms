edd63755dbb2eeba895acc58fa9e1f15d4383b41
fahim8921 <fahim.bilal.ch@gmail.com>
2022-02-08 16:40:37 +0500

    faheem

--- app/Models/attendance.php
+++ app/Models/attendance.php
@@ -5,13 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class attendance extends Model
+class Attendance extends Model
 {
-   
     use HasFactory;
-   
     protected $guarded = [
-        
-                'id'
+        'id'
     ];
 }
