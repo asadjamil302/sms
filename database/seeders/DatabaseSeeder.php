8feda79f5fd01f52d5556ba94ab39f82c2fb7c42
magesticrobot <abdulrauf735@gmail.com>
2022-02-08 18:59:23 +0500

    'changes_by_rauf'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\studentSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,7 +17,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         // $this->call(subjectSeeder::class);
         $this->call([
-            subjectSeeder::class
+            subjectSeeder::class,
+            studentSeeder::class,
         ]);   
     }
 }
