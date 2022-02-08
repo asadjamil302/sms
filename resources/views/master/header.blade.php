d7d59c55f97a36632368f8aec07ac36a8c08ae9a
fahim8921 <fahim.bilal.ch@gmail.com>
2022-02-08 18:41:37 +0500

    faheem

--- resources/views/master/header.blade.php
+++ resources/views/master/header.blade.php
@@ -114,9 +114,15 @@
                     <!-- END Side Overlay -->
 
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="op_auth_signin.html">
-                        <i class="si si-logout mr-5"></i> Sign Out
-                    </a>
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                                  document.getElementById('logout-form').submit();">
+                     {{ __('Logout') }}
+                 </a>
+
+                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                     @csrf
+                 </form>
                 </div>
             </div>
             <!-- END User Dropdown -->
